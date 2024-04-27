0e86fa6c2d3ccf741b6e5c51b3996357f54a3768
Johnson <runjiazhang.nz@gmail.com>
2024-04-27 16:36:54 +1200

    Refactor App.jsx to update ingredient list and steps in Recipe component

--- code/2-component-data/start/src/App.jsx
+++ code/2-component-data/start/src/App.jsx
@@ -17,9 +17,19 @@ function App() {
             { name: '3 potatoes, cut into 1/2" pieces', prepared: false },
             { name: '4 Tbsp butter', prepared: false },
             { name: '1/8 cup heavy cream', prepared: false },
-            { name: 'Salt', prepared: false },
-            { name: 'Pepper', prepared: false }
-        ]
+            { name: 'Salt', prepared: true },
+            { name: 'Pepper', prepared: true }
+        ],
+        steps: [
+            'Add cut potatoes to a pot of heavily salted water.',
+            'Bring pot to a boil.',
+            'Boil the potatoes until fork tender, about 15-20 minutes.',
+            'Strain the potatoes.',
+            'Return potatoes to pot.',
+            'Add butter, cream, salt, and pepper to taste.',
+            'Mash potatoes.',
+            'Reseason and add butter and cream as desired.'
+        ],
     };
     return (
         <article>
