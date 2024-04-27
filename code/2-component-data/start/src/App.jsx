4fcd54d761337c352bf2d7735791db4087be5caf
Johnson <runjiazhang.nz@gmail.com>
2024-04-27 16:28:06 +1200

    Update RecipeTitle component to include feedback prop

--- code/2-component-data/start/src/App.jsx
+++ code/2-component-data/start/src/App.jsx
@@ -25,7 +25,7 @@ function App() {
         <article>
             <h1>Recipe Manager</h1>
             {/* TODO: Add RecipeTitle component */}
-            <RecipeTitle title={recipe.title} />
+            <RecipeTitle title={recipe.title} feedback={recipe.feedback}/>
             {/* TODO: Add IngredientList component */}
 
         </article>
---
+++ code/2-component-data/start/src/IngredientList.jsx
@@ -0,0 +1,12 @@
+import './IngredientList.css';
+import React from 'react';
+
+function IngredientList(props) {
+    const ingredientsListItems = props.ingredients.map((ingredient, index) => {
+      return (
+        <li key={index} className={ingredient.prepared ? 'prepared' : ''}>
+          {ingredient.name}
+          </li>
+    );
+});
+
